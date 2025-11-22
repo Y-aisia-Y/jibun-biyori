e3720db8239dd0d4262cb7a2c9259adc3eba05ea
AK <akarikuwahara63@gmail.com>
2025-11-23 01:12:05 +0900

    feat:Devise導入による認証機能とアクセス制御の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.unique.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+
+    trait :invalid do
+      email { nil }
+    end
+  end
+end
