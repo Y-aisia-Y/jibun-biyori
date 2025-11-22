227a458385918692f400e6c379ec4362dfb35d7a
AK <akarikuwahara63@gmail.com>
2025-11-23 02:54:51 +0900

    fix: Renderデプロイ時のエラーを修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.20
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
-pin "@hotwired/stimulus-loading", to: "https://ga.jspm.io/npm:@hotwired/stimulus-loading@1.0.1/dist/stimulus-loading.js"
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.20
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.100
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
