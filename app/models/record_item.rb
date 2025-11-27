facea0bcbf3012f577d33356a00dd9cc2c063092
AK <akarikuwahara63@gmail.com>
2025-11-27 22:59:41 +0900

    feat:記録項目の初期データを投入

--- app/models/record_item.rb
+++ app/models/record_item.rb
@@ -1,5 +1,16 @@
 class RecordItem < ApplicationRecord
     
+  module InputType
+    # 数値入力
+    NUMBER = 1
+    # 選択肢からの単一選択
+    SELECT = 2
+    # テキスト入力
+    TEXT = 3
+    # 評価スライダー
+    RATING = 4
+  end
+
   has_many :record_values, dependent: :destroy
   has_many :user_record_items, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 記録項目の初期データ
+puts 'Destroying existing RecordItems...'
+
+RecordItem.destroy_all
+TYPES = RecordItem::InputType
+
+# 初期レコード項目を定義
+initial_items = [
+  {
+    name: '疲労感',
+    input_type: TYPES::RATING,
+    is_default_visible: true,
+  },
+  {
+    name: '意欲',
+    input_type: TYPES::RATING,
+    is_default_visible: true,
+  },
+  {
+    name: '睡眠時間',
+    input_type: TYPES::NUMBER,
+    is_default_visible: true,
+  },
+  {
+    name: '気分',
+    input_type: TYPES::SELECT,
+    is_default_visible: true,
+  },
+  {
+    name: 'フリーメモ',
+    input_type: TYPES::TEXT,
+    is_default_visible: false,
+  }
+]
+
+initial_items.each do |item_data|
+  RecordItem.create!(item_data)
+end
+
+puts "Created #{RecordItem.count} RecordItems."
