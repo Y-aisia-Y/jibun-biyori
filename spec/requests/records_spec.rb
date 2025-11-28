a8b9334ee14276af20b9b04f790861f82536f3ad
AK <akarikuwahara63@gmail.com>
2025-11-28 23:58:27 +0900

    Records機能の実装ほぼ完了（微調整・動作確認前）

--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -1,29 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Records", type: :request do
-  # ログイン済みのユーザーを作成
-  let(:user) { create(:user) }
-
-  # 1. ログイン済みユーザーの場合のテスト
-  describe "GET /records (index) - ログイン済み" do
-    before do
-      sign_in user 
-    end
-
-    it "HTTPステータスが200 (Success) であること" do
-      # records_path は config/routes.rb の 'as: :records' に対応
-      get records_path 
-      # indexアクションとビューが正常に表示されるため、200 OKを期待
-      expect(response).to have_http_status(:ok) 
+  describe "GET /index" do
+    it "returns http success" do
+      get "/records/index"
+      expect(response).to have_http_status(:success)
     end
   end
 
-  # 2. 未ログインユーザーの場合のテスト
-  describe "GET /records (index) - 未ログイン" do
-    it "ログインページへリダイレクトされること (302 Found)" do
-      get records_path
-      expect(response).to redirect_to(new_user_session_path)
-      expect(response).to have_http_status(302)
+  describe "GET /show" do
+    it "returns http success" do
+      get "/records/show"
+      expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/records/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/records/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
