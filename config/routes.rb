22725a6da3c9f9b0277b85e1c02368375436682e
AK <akarikuwahara63@gmail.com>
2025-12-02 23:50:59 +0900

    feat:トップページのログイン状態による切り替え

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-
   get "up" => "rails/health#show", as: :rails_health_check
 
   devise_for :users
 
   resources :records
   
-  root 'pages#top'
+  get 'welcome', to: 'welcome#index', as: :welcome
+
+  # ログイン済み
+  authenticated :user do
+    root 'pages#top', as: :authenticated_root
+  end
+
+  # 未ログイン
+  unauthenticated do
+    root 'welcome#index', as: :unauthenticated_root
+  end
 end
