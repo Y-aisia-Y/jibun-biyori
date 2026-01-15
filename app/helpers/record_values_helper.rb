caabd1f9118a2e70b30c1ea372f12d3346dc1241
AK <akarikuwahara63@gmail.com>
2026-01-16 00:24:42 +0900

    refactor: system/custom 項目の責務を分離し controller・view・helper を整理

--- app/helpers/record_values_helper.rb
+++ app/helpers/record_values_helper.rb
@@ -1,2 +1,6 @@
 module RecordValuesHelper
+  def record_value_for(record, item)
+    record.record_values.find { |rv| rv.record_item_id == item.id } ||
+      record.record_values.build(record_item: item)
+  end
 end
